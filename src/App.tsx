8765c1beb361d6027d51c3274f177b1a41abbea3
Abdullah Elsawy <abdallah666mo@gmail.com>
2026-06-23 02:46:20 +0300

    000

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import HeroSection from "./sections/HeroSection";
 import ExperienceSection from "./sections/ExperienceSection";
 import ProjectsSection from "./sections/ProjectsSection";
 import EducationSection from "./sections/EducationSection";
+import CertificatesSection from "./sections/CertificatesSection";
 import MarqueeSection from "./sections/MarqueeSection";
 import AboutSection from "./sections/AboutSection";
 import ServicesSection from "./sections/ServicesSection";
@@ -14,6 +15,7 @@ function App() {
       <ExperienceSection />
       <ProjectsSection />
       <EducationSection />
+      <CertificatesSection />
       <MarqueeSection />
       <AboutSection />
       <ServicesSection />
